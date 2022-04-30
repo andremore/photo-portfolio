f4c3b56161d5f050e188a4971c9d0dc60f11b862
André Morgado <andremorgado.work@gmail.com>
2022-04-30 17:21:13 +0100

    [NEW] Contact form with react-hook-form

--- frontend/src/components/MainApp/Contact/Contact.js
+++ frontend/src/components/MainApp/Contact/Contact.js
@@ -1,5 +1,50 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export const Contact = () => {
-    return <div>Contact</div>;
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {/* Name */}
+            <label htmlFor="name">Name</label>
+            <input
+                {...register('Name', { required: true })}
+                type="text"
+                required
+                placeholder="Name"
+                id="name"
+            />
+            {/* Email */}
+            <label htmlFor="email">Email</label>
+            <input
+                {...register('Email', { required: true })}
+                type="email"
+                required
+                placeholder="Email"
+                id="email"
+            />
+            {/* Subject */}
+            <label htmlFor="subject">Subject</label>
+            <input
+                {...register('Subject', { required: true })}
+                required
+                type="text"
+                placeholder="Subject"
+                id="subject"
+            />
+            {/* Message */}
+            <label htmlFor="message">Message</label>
+            <textarea
+                {...register('Message', { required: true })}
+                required
+                type="text"
+                placeholder="Message"
+                id="message"
+                className="textarea textarea-bordered w-full"
+            />
+            {/* Submit */}
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
--- frontend/src/components/MainApp/Navigation/Navbar.js
+++ frontend/src/components/MainApp/Navigation/Navbar.js
@@ -2,13 +2,11 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <nav className="text-gray-200 navbar">
-            <div className="navbar-start">
-                <Link to="/">Home</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact me</Link>
-            </div>
+        <nav className="text-gray-200">
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact me</Link>
         </nav>
     );
 };
